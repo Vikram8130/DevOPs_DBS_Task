4b1614100c928f6b0cae8f070e045d41afe90d4c
Vikramaditya Karanwal <adityavkrm00@gmail.com>
2024-11-12 05:06:41 +0530

    New Changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,18 +12,20 @@ sudo docker tag google-checker google-checker:latest
 
 function deploy() {
     echo "Deploying application..."
-    kubectl apply -f Kubernetes/deployment.yaml
-    kubectl apply -f Monitoring/prometheus-configmap.yaml
-    kubectl apply -f Monitoring/prometheus-deployment.yaml
-    kubectl apply -f Monitoring/grafana-deployment.yaml    
+    kubectl apply -f Kubernetes/deployment.yaml -n status-checker
+    kubectl apply -f Monitoring/prometheus-configmap.yaml -n monitoring
+    kubectl apply -f Monitoring/prometheus-deployment.yaml -n monitoring
+    kubectl apply -f Monitoring/grafana-deployment.yaml -n monitoring
+    kubectl apply -f Monitoring/grafana-datasource-prometheus-configmap.yaml -n monitoring    
 }
 
 function teardown() {
     echo "Tearing down application..."
-    kubectl delete -f Kubernetes/deployment.yaml
-    kubectl delete -f Monitoring/prometheus-configmap.yaml
-    kubectl delete -f Monitoring/prometheus-deployment.yaml
-    kubectl delete -f Monitoring/grafana-deployment.yaml
+    kubectl delete -f Kubernetes/deployment.yaml -n status-checker
+    kubectl delete -f Monitoring/prometheus-configmap.yaml -n monitoring
+    kubectl delete -f Monitoring/prometheus-deployment.yaml -n monitoring
+    kubectl delete -f Monitoring/grafana-deployment.yaml -n monitoring
+    kubectl delete -f Monitoring/grafana-datasource-prometheus-configmap.yaml -n monitoring
 }
 
 if [ "$1" == "start" ]; then
